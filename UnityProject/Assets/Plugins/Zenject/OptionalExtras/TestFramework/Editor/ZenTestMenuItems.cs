7dc7175a82277bd1333accbf2e988ea207361b1e
Steve Vermeulen <sfvermeulen@gmail.com>
2018-08-26 12:21:40 +0800

    Fixed editor window to use virtual methods like previously

--- UnityProject/Assets/Plugins/Zenject/OptionalExtras/TestFramework/Editor/ZenTestMenuItems.cs
+++ UnityProject/Assets/Plugins/Zenject/OptionalExtras/TestFramework/Editor/ZenTestMenuItems.cs
@@ -15,7 +15,7 @@ namespace Zenject.Internal
         [MenuItem("Assets/Create/Zenject/Unit Test", false, 60)]
         public static void CreateUnitTest()
         {
-            ZenMenuItems.AddCSharpClassTemplate("Unit Test", "UntitledUnitTest", true,
+            ZenMenuItems.AddCSharpClassTemplate("Unit Test", "UntitledUnitTest", 
                   "using Zenject;"
                 + "\nusing NUnit.Framework;"
                 + "\n"
@@ -33,7 +33,7 @@ namespace Zenject.Internal
         [MenuItem("Assets/Create/Zenject/Integration Test", false, 60)]
         public static void CreateIntegrationTest()
         {
-            ZenMenuItems.AddCSharpClassTemplate("Integration Test", "UntitledIntegrationTest", false,
+            ZenMenuItems.AddCSharpClassTemplate("Integration Test", "UntitledIntegrationTest", 
                   "using Zenject;"
                 + "\nusing System.Collections;"
                 + "\nusing UnityEngine.TestTools;"
@@ -61,7 +61,7 @@ namespace Zenject.Internal
         [MenuItem("Assets/Create/Zenject/Scene Test", false, 60)]
         public static void CreateSceneTest()
         {
-            ZenMenuItems.AddCSharpClassTemplate("Scene Test Fixture", "UntitledSceneTest", false,
+            ZenMenuItems.AddCSharpClassTemplate("Scene Test Fixture", "UntitledSceneTest", 
                   "using Zenject;"
                 + "\nusing System.Collections;"
                 + "\nusing UnityEngine;"
