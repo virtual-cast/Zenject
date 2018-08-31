3ad953c8ebd539105b76dbcf38c2ba5882755e3a
Steve Vermeulen <sfvermeulen@gmail.com>
2018-08-31 14:29:33 +0800

    Added ability to use custom attributes other than zenjects InjectAttribute (issue #536)

--- UnityProject/Assets/Plugins/Zenject/OptionalExtras/UnitTests/Editor/Other/TestBindCallbacks.cs
+++ UnityProject/Assets/Plugins/Zenject/OptionalExtras/UnitTests/Editor/Other/TestBindCallbacks.cs
@@ -28,6 +28,10 @@ namespace Zenject.Tests.Other
             public class Factory : PlaceholderFactory<Foo>
             {
             }
+
+            public class Pool : MemoryPool<Foo>
+            {
+            }
         }
 
         [Test]
@@ -58,6 +62,22 @@ namespace Zenject.Tests.Other
                 });
 
             var foo = Container.Resolve<Foo.Factory>().Create();
+
+            Assert.IsEqual(foo.Value, "asdf");
+        }
+
+        [Test]
+        public void TestMemoryPool1()
+        {
+            Container.BindInstance(5).WhenInjectedInto<Foo>();
+
+            Container.BindMemoryPool<Foo, Foo.Pool>().OnInstantiated<Foo>((ctx, f) =>
+                {
+                    Assert.IsEqual(f.Value2, 5);
+                    f.Value = "asdf";
+                });
+
+            var foo = Container.Resolve<Foo.Pool>().Spawn();
 
             Assert.IsEqual(foo.Value, "asdf");
         }
