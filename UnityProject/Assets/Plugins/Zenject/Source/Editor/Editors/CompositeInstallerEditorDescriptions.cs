3e69453f03edf8845c03f346e7bd93547a9f4d38
tsgcpp <tsgcpp.public8x@gmail.com>
2021-02-11 10:53:29 +0900

    Fix the message of "ErrorTooltip" in "CompositeInstallerEditorDescriptions"

--- UnityProject/Assets/Plugins/Zenject/Source/Editor/Editors/CompositeInstallerEditorDescriptions.cs
+++ UnityProject/Assets/Plugins/Zenject/Source/Editor/Editors/CompositeInstallerEditorDescriptions.cs
@@ -2,6 +2,6 @@ namespace Zenject
 {
     public static class CompositeInstallerEditorDescriptions
     {
-        public const string ErrorTooltip = "WARNING: This composite installer has some circular references and will cause \"StackOverflowException\"";
+        public const string ErrorTooltip = "WARNING: This composite installer has some circular references and will cause an exception";
     }
 }
